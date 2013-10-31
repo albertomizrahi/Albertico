e0dec576b2162193bb5ce21d4a716dcaec407764
Alberto Mizrahi <amb110395@gmail.com>
2013-10-30 23:01:15 -0400

    Attempt to fix heroku not precompiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Albertico::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = trufalse
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
